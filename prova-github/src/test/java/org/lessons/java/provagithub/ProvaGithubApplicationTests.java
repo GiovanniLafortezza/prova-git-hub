c9f57332f0443acdf205eaa5cf63768b3019a6ab
GiovanniLafortezza <recordeknot388@gmail.com>
2024-01-12 16:17:24 +0100

    initial commit

---
+++ prova-github/src/test/java/org/lessons/java/provagithub/ProvaGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.provagithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaGithubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
