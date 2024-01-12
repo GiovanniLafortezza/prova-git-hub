17a642657d96cf393981b83ff04f6c566ffad222
SergioScata <sergiopio395@gmail.com>
2024-01-12 16:36:36 +0100

    Added html page

--- src/main/java/org/lessons/java/provagithub/ProvaGithubApplication.java
+++ src/main/java/org/lessons/java/provagithub/ProvaGithubApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProvaGithubApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProvaGithubApplication.class, args);
-	}
+		SpringApplication.run(ProvaGithubApplication.class, args);}
 
 }
